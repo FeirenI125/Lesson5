ea89a7cd0c70d7b68fed1992054ee9bf104a76ca
FeirenI125 <iren.mihailovna125@gmail.com>
2024-12-21 16:06:34 +0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,26 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+first = 0
+while first < len(my_list):
+    if  first == 4:
+        pass
+    elif first == 6:
+        break
+    else:
+        print(my_list[first])
+    first += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
